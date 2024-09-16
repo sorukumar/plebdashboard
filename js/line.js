4d79d0188aec33423e086392d014d05cbd45ddb7
Mars <marishasahay@gmail.com>
2024-09-16 10:15:32 -0600

    Update line.js

--- js/line.js
+++ js/line.js
@@ -148,7 +148,7 @@ function drawNodeChart(data, tooltipData) {
                 datalabels: {
                     color: '#fff',
                     font: { weight: 'bold' },
-                    formatter: (value, context) => value > 5 ? value.toFixed(1) + '%' : ''
+                    formatter: (value, context) => value > 0.1 ? value.toFixed(1) + '%' : ''
                 }
             }
         }
