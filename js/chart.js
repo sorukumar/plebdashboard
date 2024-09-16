524ef49a8fd67c5c0c1c655995711228e58035fe
Mars <marishasahay@gmail.com>
2024-09-16 10:08:48 -0600

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -128,7 +128,7 @@ function drawChart(data, aggregateData) {
                     font: {
                         weight: 'bold'
                     },
-                    formatter: (value, context) => value > 5 ? value.toFixed(1) + '%' : ''
+                    formatter: (value, context) => value > 0.1 ? value.toFixed(1) + '%' : ''
                 }
             }
         },
